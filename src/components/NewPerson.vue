9695ec62647f53abb0f5a800fb11977ad3b08545
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-10-24 13:14:09 +0200

    default img updated

--- src/components/NewPerson.vue
+++ src/components/NewPerson.vue
@@ -3,7 +3,7 @@
         <div class="card offset-2 col-md-3">
             <div class="card-body tex-center d-flex align-items-center flex-column">
                 <img height="128" class="img-responsive text-center mb-3"
-                     :src="person.selectedImage == null ? './default.png' : person.selectedImage">
+                     :src="person.selectedImage == null ? '../assets/default.png' : person.selectedImage">
                 <input ref="file" type="file" style="display: none;" @change="onChange($event)" class="form-control">
                 <button class="btn btn-outline-secondary " type="button" @click="$refs.file.click()">Profile picture</button>
             </div>
